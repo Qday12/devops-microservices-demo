fae2b014dd1872819fdaaf12dbc2f590d0f4a637
Qday12 <Blazejowski20@gmail.com>
2025-09-19 17:12:56 +0200

    Update Terraform configuration and add Kubernetes manifests for backend service and ingress

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,19 +1,26 @@
 provider "aws" {
-  region = "eu-central-1"  # zmień na swój region
+  region = "eu-north-1"  # zmień na swój region
 }
 
 # Security Group
 resource "aws_security_group" "backend_sg" {
   name        = "backend-sg"
-  description = "Allow HTTP traffic to backend"
-
+  description = "Allow HTTP + SSH traffic to backend"
+  vpc_id      = "vpc-0637b2eb44d8228d3"
+  # backend port
   ingress {
     from_port   = 5000
     to_port     = 5000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  # SSH access
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
@@ -24,33 +31,34 @@ resource "aws_security_group" "backend_sg" {
 
 # EC2 instance z userdata
 resource "aws_instance" "backend" {
-  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2
+  ami           = "ami-0068163775a114e89"
   instance_type = "t3.micro"
   key_name      = var.key_name
   vpc_security_group_ids = [aws_security_group.backend_sg.id]
 
-  user_data = <<-EOF
+user_data = <<-EOF
               #!/bin/bash
-              # Aktualizacja systemu
-              yum update -y
-              
-              # Instalacja Dockera
-              amazon-linux-extras enable docker
-              yum install -y docker
+              # Update system
+              apt-get update -y
+              apt-get upgrade -y
+
+              # Install Docker
+              apt-get install -y docker.io docker-compose git
+
+              # Enable Docker
               systemctl enable docker
               systemctl start docker
-              usermod -aG docker ec2-user
-              
-              # Instalacja Docker Compose
-              curl -L "https://github.com/docker/compose/releases/download/v2.29.7/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-              chmod +x /usr/local/bin/docker-compose
-              
-              # Klonowanie repozytorium
-              su - ec2-user -c "git clone https://github.com/qday12/devops-microservices-demo.git ~/devops-microservices-demo"
-              
-              # Uruchomienie backendu
-              cd /home/ec2-user/devops-microservices-demo
-              su - ec2-user -c "docker-compose up -d"
+
+              # Add ubuntu user to docker group
+              usermod -aG docker ubuntu
+
+              # Clone repository
+              git clone https://github.com/qday12/devops-microservices-demo.git /home/ubuntu/devops-microservices-demo
+              chown -R ubuntu:ubuntu /home/ubuntu/devops-microservices-demo
+
+              # Run backend
+              cd /home/ubuntu/devops-microservices-demo
+              sudo -u ubuntu docker-compose up -d
               EOF
 
   tags = {
