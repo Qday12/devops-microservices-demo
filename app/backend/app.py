2a1736bdc3eda1a6d42c5e8eb45ed45df7b342c7
Qday12 <Blazejowski20@gmail.com>
2025-09-30 13:21:49 +0200

    Adds Prometheus setup

--- app/backend/app.py
+++ app/backend/app.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
+
+app = Flask(__name__)
+metrics = PrometheusMetrics(app)
 
 app = Flask(__name__)
 
@@ -12,7 +16,7 @@ def users():
 
 @app.route("/")
 def helloworld():
-    return "Hello, Tomek!"
+    return "Hello, World!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
